913fa4592cfe7256f36cd6a62d865c592b27b23a
Yamila Ramos <ramosyamila64@gmail.com>
2023-11-04 18:22:41 -0300

    Cambios Grandes

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ let value = 0
 // trail index number
 let trailValue = 0
 // interval (Duration)
-let interval = 4000
+let interval = 4500
 
 // Function to slide forward
 const slide = (condition) => {
@@ -56,7 +56,7 @@ const tl = gsap.timeline({defaults: {duration: 0.6, ease: "power2.inOut"}})
 tl.from(".fondo", {x: "-100%", opacity: 0})
   .from("p", {opacity: 0}, "-=0.3")
   .from("h1", {opacity: 0, y: "30px"}, "-=0.3")
-  .from("button", {opacity: 0, y: "-40px"}, "-=0.8")
+  .from("button", {opacity: 7, y: "-40px"}, "-=0.8")
 
 // function to restart animation
 const animate = () => tl.restart()
